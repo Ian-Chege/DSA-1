c896e591d8aba592b6f679c61ccf3464ee0c5f2c
Ian Chege <ianchege001@gmail.com>
2024-08-23 20:25:00 +0300

    iterative sorts

--- specs/insertion-sort/insertion-sort.test.js
+++ specs/insertion-sort/insertion-sort.test.js
@@ -14,12 +14,36 @@
 */
 
 function insertionSort(nums) {
-  // code goes here
+  // Outer loop: Start from the second element (index 1) since the first element is trivially "sorted".
+  for (let i = 1; i < nums.length; i++) {
+    // The 'key' is the current element that we need to insert into the correct position
+    // within the sorted portion of the array.
+    let key = nums[i];
+
+    // Initialize 'j' to be the index of the last element in the sorted portion of the array.
+    // This helps us compare the 'key' with elements in the sorted part.
+    let j = i - 1;
+
+    // Inner loop: Shift elements in the sorted portion of the array to the right
+    // as long as they are greater than the 'key'.
+    // This loop moves larger elements one position to the right to make space for the 'key'.
+    while (j >= 0 && nums[j] > key) {
+      // Move the current element at index 'j' to the position 'j + 1'
+      nums[j + 1] = nums[j];
+      // Decrement 'j' to continue checking the next element to the left.
+      j--;
+    }
+
+    // Insert the 'key' into its correct position in the sorted portion.
+    // 'j + 1' is now the correct index where 'key' should be placed.
+    nums[j + 1] = key;
+  }
+  return nums;
 }
 
 // unit tests
 // do not modify the below code
-test.skip("insertion sort", function () {
+test("insertion sort", function () {
   const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
   insertionSort(nums);
   expect(nums).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
